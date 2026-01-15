42452e81ae28ed7bbac52b6692d36e46e9db6374
Allah Ditta <97169517+allahdittaiu@users.noreply.github.com>
2026-01-15 18:59:26 +0500

    Day-1

---
+++ day1.py
@@ -0,0 +1,27 @@
+print("Hello, python world!")
+print(10)
+print(5 + 3)
+# This is a comment
+print("Hi, This is Allah Ditta")
+print("I am learning Python programming.")
+print("Python is fun!")
+
+print("I love coding.")
+print("Let's write some Python code.")  
+
+# Age
+print(24)
+# Addition
+print(14 + 10)
+# Greeting
+print("Welcome to Python programming!")
+print("Python is awesome!")
+print("Coding is enjoyable.")
+print("Let's build something amazing with Python!")
+print("Python is easy to learn and fun to use.")
+
+# Final message
+print("Happy coding!")
+
+# minus operation
+print(20 - 5)
