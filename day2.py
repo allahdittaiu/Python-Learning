58f4b859bd013c658a793c2731bdad7770da77a2
Allah Ditta <97169517+allahdittaiu@users.noreply.github.com>
2026-01-20 12:47:36 +0500

    Update Day-2

---
+++ day2.py
@@ -0,0 +1,57 @@
+
+# variables are used to store data in a program like age, name, height, etc.
+
+_age = 24  # integer (whole number)
+print(_age)
+
+_age = 25
+print(_age)
+
+_name = "Allah Ditta"  # string (text)
+print(_name)
+height = 5.10  # float (decimal number)
+print(height)
+
+is_student = False  # boolean (True or False)
+print(is_student)
+
+# print the type of the variables
+print(type(_age))
+print(type(_name))
+print(type(height))
+print(type(is_student))
+
+# variables can be named anything, but they cannot start with a number
+_1 = 10
+print(_1)
+print(type(_1))
+
+Capital_Letter = "AD"
+print(Capital_Letter)
+
+user_1 = "Allah Ditta"
+print(user_1)
+
+classs = "Python"
+print(classs)
+
+
+# Day 2 Practice Questions
+
+name = "Allah Ditta"
+print(name)
+
+age = 24
+print(age)
+
+a =10
+b = 3
+print(a + b)
+
+is_learning = True
+print(is_learning)
+
+print(type(name))
+print(type(age))
+print(type(a + b))
+print(type(is_learning))
